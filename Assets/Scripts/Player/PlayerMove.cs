36345af5a47fe34cc0c9fe2d9da1dc256f206a6f
loc20904 <nguyenthanhloc20092004@gmail.com>
2026-01-08 09:40:01 +0700

    FuncMovement_Loc20904_08012026: Add mix file Player_move.cs

---
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+public class PlayerMove : MonoBehaviour
+{
+    @Player_Move controls;
+    Vector2 moveInput;
+
+    void Awake()
+    {
+        controls = new @Player_Move();
+
+        controls.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        controls.Player.Jump.performed += ctx => Jump();
+    }
+
+    void OnEnable() => controls.Player.Enable();
+    void OnDisable() => controls.Player.Disable();
+
+    void Update()
+    {
+        Debug.Log("Move = " + moveInput);
+    }
+
+    void Jump()
+    {
+        Debug.Log("Jump");
+    }
+}
